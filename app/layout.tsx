3f5919e4e1baa63e88089bf4a1023e5e3d9013d1
Umair Azmat <devumair260@gmail.com>
2026-02-10 10:17:16 +0500

    Google Tag manager Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -152,7 +152,28 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           />
         )}
         {/* End Google Tag Manager */}
-        
+        {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+          <>
+            <Script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script
+              id="google-analytics"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+                `,
+              }}
+            />
+          </>
+        )}
         <StructuredData />
       </head>
       <body className={`${inter.variable} ${roboto.variable} font-sans`}>
